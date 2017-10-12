3905432df685b53bb631d1c6390189af009e8ec9
David Van Isacker <davidvanisacker@gmail.com>
2017-10-12 19:33:33 +0900

    integrated dat-node

--- main.js
+++ main.js
@@ -1,38 +1,47 @@
 const electron = require('electron')
 
 const app = electron.app
-
 const BrowserWindow = electron.BrowserWindow
 
+
+const fs = require('fs')
 const path = require('path')
 const url = require('url')
-
 const ram = require('random-access-memory')
 const hyperdrive = require('hyperdrive')
 const discovery = require('hyperdiscovery')
 const mirror = require('mirror-folder')
+const Dat = require('dat-node')
 
 var link = process.argv[2]
 var dir = process.cwd()
 
 let mainWindow
 
-function createWindow () {
+var key = process.argv[2]
+if (!key) {
+  console.log('Run with: electron ./ <key>')
+  process.exit(1)
+}
+
+var dest = path.join(__dirname, 'tmp')
+fs.mkdirSync(dest)
+
+
+function createWindow() {
+  Dat(ram, {key: key, sparse: true}, function(err, dat) {
+    if (err) throw err
+
+    var network = dat.joinNetwork()
+    network.once('connection', function(){
+      console.log('Connected')
+    })
+
+    dat.archive.metadata.update(download)
+    console.log(`Downloading: ${dat.key.toString('hex')}\n`)
+  })
 
   mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  var archive = hyperdrive(ram, link)
-  archive.ready(function() {
-    discovery(archive)
-
-    var progress = mirror({ name: '/download/', fs: archive}, dir, function(err) {
-      console.log('done downloading')
-    })
-
-    progress.on('put', function(src) {
-      console.log(src.name, 'downloaded')
-    })
-  })
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
@@ -42,6 +51,18 @@ function createWindow () {
 
   mainWindow.on('closed', function () {
     mainWindow = null
+  })
+}
+
+
+function download() {
+  var progress = mirror({ fs: dat.archive, name: '/'}, dest, function(err) {
+    if (err) throw err
+    console.log('Done')
+  })
+
+  progress.on('put', function(src) {
+    console.log('Downloading', src.name)
   })
 }
 
